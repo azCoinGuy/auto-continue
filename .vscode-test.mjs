71a0e1c0752c056a6366f7eb0b84ab2e73042a6b
azCoinGuy <azcoinguy@protonmail.com>
2025-07-18 00:09:29 -0700

    readme.md updates (#2)

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -2,4 +2,8 @@ import { defineConfig } from '@vscode/test-cli';
 
 export default defineConfig({
 	files: 'out/test/**/*.test.js',
+	workspaceFolder: './src/test/fixtures',
+	extensionDevelopmentPath: '.',
+	extensionTestsPath: './out/test',
+	userDataDir: '/tmp/vscode-auto-continue-test'
 });
